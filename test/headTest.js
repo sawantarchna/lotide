f54bf9f75c6392c05d63c94d51c952d19ee772df
Archana Deulkar <88867952+sawantarchna@users.noreply.github.com>
2021-11-30 11:19:12 -0500

    test mocha chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,16 +1,28 @@
 // ASSERT EQUAL...
-const assertEqual = require('./assertEqual').default;
+// const assertEqual = require('./assertEqual').default;
 
 
-// HEAD FUNCTION
-const head = (array) => {
-  if (array.length >= 1) {
-    let headElement = array[0];
-    return headElement;
-  } else if (array.length === 0) {
-    return undefined;
-  }
-};
+// // HEAD FUNCTION
+// const head = (array) => {
+//   if (array.length >= 1) {
+//     let headElement = array[0];
+//     return headElement;
+//   } else if (array.length === 0) {
+//     return undefined;
+//   }
+// };
 
 
-module.exports = head;
+// module.exports = head;
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
+it("returns '5' for ['5']", () => {
+  assert.strictEqual(head(['5']), '5'); 
+});
